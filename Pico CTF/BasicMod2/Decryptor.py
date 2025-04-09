f684ff50dc45784eb26bde9306307f323db9eeca
Haippp <haiperdesain@gmail.com>
2025-04-09 18:39:32 +0800

    Making coding too look pretty

--- Pico CTF/BasicMod2/Decryptor.py
+++ Pico CTF/BasicMod2/Decryptor.py
@@ -1,9 +1,8 @@
+# Text flag
 flag = "104 372 110 436 262 173 354 393 351 297 241 86 262 359 256 441 124 154 165 165 219 288 42"
-mod = [x for x in "abcdefghijklmnopqrstuvwxyz0123456789_"]
-Decrypt = ""
-
-for n in flag.split():
-    index = pow(int(n), -1, 41) - 1
-    Decrypt += mod[index]
-
+# Arayy Karakter Set 
+KarakterSet = [x for x in "abcdefghijklmnopqrstuvwxyz0123456789_"]
+# Variable untuk menyimpan sekaligus melakukan dekripsinya
+Decrypt = "".join(KarakterSet[pow(int(n), -1, 41) - 1] for n in flag.split())
+# Menampilkan hasil dekripsi dan membungkusnya kedalam flag picoCTF
 print('picoCTF{'+ Decrypt + '}')
